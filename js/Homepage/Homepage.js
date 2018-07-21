b5481789dd3860694c5fe3978850c87078beb8a8
marbuda <a8862688626@gmail.com>
2018-07-22 01:37:42 +0800

    doesn't work properly ;(

--- js/Homepage/Homepage.js
+++ js/Homepage/Homepage.js
@@ -1,5 +1,21 @@
 $(function(){
-    $('#menu').load('/view/Homepage/menu.html');
+    $('#menu').load('/view/Homepage/menu1.html');
     $('#type1').load('/view/Homepage/type1.html');
     $('#type2').load('/view/Homepage/type2.html');
-})
+//捲動瀏覽器
+    var navPos = $("nav").offset().top;
+
+	$(window).scroll(function(event) {
+		if ($(window).scrollTop() > navPos) {
+			$("nav").css("position", 'fixed');
+		}
+		else{
+			$("nav").css("positiion",'static');
+		}
+	});
+//捲動瀏覽器
+
+    });
+    
+
+
