9e39c4fafd93dc0dadcceeb41a469982b28fa1de
empire5132 <empire5132@hotmail.com>
2018-07-23 00:36:51 +0800

    Fix syntax error and coding style

--- js/Homepage/Homepage.js
+++ js/Homepage/Homepage.js
@@ -2,21 +2,19 @@ $(function(){
     $('#menu').load('/view/Homepage/menu1.html');
     $('#type1').load('/view/Homepage/type1.html');
     $('#type2').load('/view/Homepage/type2.html');
-//捲動瀏覽器
-    var navPos = $("nav").offset().top;
 
-	$(window).scroll(function() {
-		if ($(window).scrollTop() > navPos) {
-			$("nav").css("position", 'fixed');
-		}
-		else{
-			$("nav").css("positiion",'static');
-			alert("Hello! I am an alert box!!");
-		}
-	});
-//捲動瀏覽器
-
+    let navPos = $('.nav').offset().top;
+    $(window).scroll(function() {
+        let windowScroll = $(window).scrollTop();
+        if (windowScroll > navPos) {
+            $('.nav').css('position', 'fixed');
+        } else {
+            $('.nav').css('position','static');
+            // alert('Hello! I am an alert box!!');
+        }
     });
+
+});
     
 
 
