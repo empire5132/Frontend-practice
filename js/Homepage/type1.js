4d270b0759973449e954de64096fa44c72bf462d
marbuda <a8862688626@gmail.com>
2018-07-29 21:36:38 +0800

    find out

--- js/Homepage/type1.js
+++ js/Homepage/type1.js
@@ -1,10 +1,6 @@
 $(function(){
     $("a").click(function(){
-        //好像是錯在這行
-        //$('#show').children('img').attr('src', '$(this).attr('href')');
-        //上面那行
-        alert($(this).attr('href'));
-
+        $('#show').children('img').attr('src',$(this).attr('href'));
 
 
        return false 
