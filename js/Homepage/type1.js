bab79e1abce3180a3bb94813a93c86c82ff6b2dd
marbuda <a8862688626@gmail.com>
2018-07-29 18:45:03 +0800

    befor dinnner

---
+++ js/Homepage/type1.js
@@ -0,0 +1,17 @@
+$(function(){
+    $("a").click(function(){
+        $("#show img").attr("src","$(this).attr("href")");
+        $(this).attr("href");
+
+
+
+
+       return false 
+    });
+
+
+
+
+
+
+})
