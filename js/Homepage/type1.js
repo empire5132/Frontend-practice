48cec0049aee0e12db30e86fad828b287f8e8a91
empire5132 <empire5132@hotmail.com>
2018-07-30 15:51:04 +0800

    Add class to gallery switch

--- js/Homepage/type1.js
+++ js/Homepage/type1.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('a').click(function(e){
+    $('.gallery').click(function(e){
         e.preventDefault();
         $('#show').children('img').attr('src', $(this).attr('href'));
 
