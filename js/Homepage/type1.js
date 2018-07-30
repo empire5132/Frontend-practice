60fff3800f517228716d296f05d237caea35fa94
marbuda <a8862688626@gmail.com>
2018-07-30 18:08:51 +0800

    helplsss

--- js/Homepage/type1.js
+++ js/Homepage/type1.js
@@ -1,4 +1,8 @@
 $(function(){
+    $('#test').load('/view/Homepage/ui1.html');
+
+
+
     $('.gallery').click(function(e){
         e.preventDefault();
         $('#show').children('img').attr('src', $(this).attr('href'));
