a96175345434fb8b7544c4228e812ba60964bfc1
marbuda <a8862688626@gmail.com>
2018-07-30 17:43:06 +0800

    name2

--- js/Homepage/type2.js
+++ js/Homepage/type2.js
@@ -1,9 +1,13 @@
 $(function(){
-    $('#info').children('section[id!="text1"]').hide();
+    $('#info').children('section[id!="graphic1"]').hide();
     $('.gallery2').click(function(e){
         e.preventDefault();
         $('#show2').children('img').attr('src', $(this).attr('href'));
-        //$('#info').
+        $('#info').children('section').hide();
+        // console.log(this);
+        let name2 = $(this).attr('href').split('/')[2].split('.')[0];
+        //console.log(name2);
+        $('#info').children('section[id='+name2+']').show();
     });
 
 
