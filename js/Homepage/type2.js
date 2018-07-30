ed5f8194a562fcb4fb34450c51180cbfdc0ae0de
empire5132 <empire5132@hotmail.com>
2018-07-30 17:27:37 +0800

    ..........

--- js/Homepage/type2.js
+++ js/Homepage/type2.js
@@ -1,5 +1,5 @@
 $(function(){
-    $('section[id!="text1"]').hide(); 
+    $('#info').children('section[id!="text1"]').hide();
     $('.gallery2').click(function(e){
         e.preventDefault();
         $('#show2').children('img').attr('src', $(this).attr('href'));
