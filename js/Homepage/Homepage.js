ca3e47c5bc754bb422fee846ce08344ee67105a5
marbuda <a8862688626@gmail.com>
2018-08-04 00:41:09 +0800

    about 08/03

--- js/Homepage/Homepage.js
+++ js/Homepage/Homepage.js
@@ -2,6 +2,7 @@ $(function(){
     $('#type1').load('/view/Homepage/type1.html');
     $('#type2').load('/view/Homepage/type2.html');
     $('#tab2').load('/view/Homepage/about.html')
+    $('#tab3').load('/view/Homepage/contact.html')
     let navPos = $('.nav').offset().top;
     $(window).scroll(function() {
         let windowScroll = $(window).scrollTop();
